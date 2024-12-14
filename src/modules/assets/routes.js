65c34ab08229eb4208aa623830e4968ef16af0b8
EricLacsamana <eric.lacsamana07@gmail.com>
2024-12-14 16:21:41 +0800

    remove comments

--- src/modules/assets/routes.js
+++ src/modules/assets/routes.js
@@ -4,19 +4,14 @@ import assetController from './controller.js';
 
 const assetRoutes = Router();
 
-// Create a new asset
 assetRoutes.post('/', authenticateJWT, assetController.createAsset);
 
-// Get all assets
 assetRoutes.get('/', authenticateJWT, assetController.getAssets);
 
-// Get a single asset by ID
 assetRoutes.get('/:id', authenticateJWT, assetController.getAsset);
 
-// Update asset by ID
 assetRoutes.put('/:id', authenticateJWT, assetController.updateAsset);
 
-// Delete asset by ID
 assetRoutes.delete('/:id', authenticateJWT, assetController.deleteAsset);
 
 export default assetRoutes;

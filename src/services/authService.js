18557c82882ea509aba8ee421c3c21b4cceb0148
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-28 19:13:06 +0800

    revert generateToken

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 const authService = {
+    generateToken: ({ id, email }) => {
+        return jwt.sign({ id, email }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        });
+    },
     generateAccessToken: (payload) => {
         return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET_KEY, {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
